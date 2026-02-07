9e7652a71fa066c704e635dede8b812c92acaa10
Gabae7 <veltrig7@gmail.com>
2026-02-06 19:01:06 -0700

    Camera and Controller

---
+++ Assets/MCassets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 4;
+    public int currentHealth;
+
+    //Add this once you hahve death animation
+    //public Animator anim;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            //Add death screen
+            //Add animation: anim.SetBool("IsDead", true);
+        }
+    }
+}
